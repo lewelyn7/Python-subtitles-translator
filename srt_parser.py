a08249d43ec615cbe4b6d59c34fda25c07dabe37
retkiewi <58555777+retkiewi@users.noreply.github.com>
2020-04-24 17:33:06 +0200

    Create srt_parser.py

---
+++ srt_parser.py
@@ -0,0 +1,43 @@
+import itertools
+
+def clean_line(line):
+    cleaned_line=""
+    for letter in line:
+        if letter not in "(<{[)>}]\"":
+            cleaned_line+=letter
+    return cleaned_line
+
+def clean_word(word):
+    cleaned_word = ""
+    if word[0]=='\'':
+        word = word[1:]
+    if word[-1]=='\'':
+        word = word[0:-1]
+    for letter in word:
+        if letter not in "-.:;,?!":
+            cleaned_word+=letter
+        elif letter not in ".:;,?!":
+            cleaned_word+=' '
+    return cleaned_word
+
+def parse_srt(srt_path):
+    srt_file = open(srt_path, "r")
+    for _ in range(3):
+        line = srt_file.readline()
+    while line!="":
+        if len(line)<2:
+            break
+        if line.find("-->") != -1:
+            break
+        line = clean_line(line)
+        words = [word for word in line.split(' ') if len(word) > 0]
+        for i in range(len(words)):
+            word = words[i]
+            if not word.isalpha():
+                break
+            if i == 0 or words[i-1][-1] in '.!?':
+                yield word
+            if word.islower():
+                yield word
+            if word.isupper():
+                yield word
