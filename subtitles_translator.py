006e9c9212667cd60566f73858b5c23de5120f54
lewelyn7 <progit8itakademia>
2020-04-28 16:01:02 +0200

    filters in progress

--- subtitles_translator.py
+++ subtitles_translator.py
@@ -1,5 +1,6 @@
 from srt_parser import parse_srt
 from csv_importer import csv_importer
+from word_filter import w_filter
 from tinydb import TinyDB, Query
 
 def db_insert_wrapper(db, word):
@@ -9,19 +10,25 @@ def db_insert_wrapper(db, word):
         print("dodano: " + word['word'])
 
 if __name__ == "__main__":
-    # srt_file = open("example_srt_file.txt", "r")
-    # srt_words = parse_srt(srt_file)
-    # for word in srt_words:
-    #     print(word)
-
     #db section
     db = TinyDB('db.json')
     # document format: {'word' : <word>, 'translation': <translation>, 'freq' : <freq>}
 
     anki = csv_importer("testAnki.csv")
+
+    fl = w_filter()
+    
+    srt_file = open("example_srt_file.txt", "r")
+    srt_words = parse_srt(srt_file)
+    for word in srt_words:
+        print(word)
+        fl.filter(word.lower())
+
+
     for word in anki.read():
         db_item = {}
-        print( word[0] + " - " + word[1])
+        # print( word[0] + " - " + word[1])
         db_item['word'] = word[0]
         db_item['translation'] = word[1]
         db_insert_wrapper(db, db_item)
+
