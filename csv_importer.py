3463e4b9ea43aa335df74e0616855bf5e5019806
lewelyn7 <progit8itakademia>
2020-04-28 12:59:04 +0200

    added csv importer for Anki, Quizlet

---
+++ csv_importer.py
@@ -0,0 +1,34 @@
+from csv import reader
+import re
+
+class csv_importer:
+    """Importer for using with Anki or Quizlet, file should be in csv format with comma as a delitmer
+    and new line to indicate new row ( new word )"""
+
+    def __init__(self, filename, header=False, firstcol="mylang"):
+        """ Constructor accepts csv filename and optional header argument and order of words in csv fie.
+         If translations are placed in first column, then firstcol should be set to "translations" """
+        try:
+            assert re.match(".*\.csv$", filename), "file has wrong extension"
+            self.filename = filename
+            self.header = header
+            self.firstcol = firstcol
+        except AssertionError as inst:
+            print("assert error", inst.args)
+    def read(self):
+        """ returns an iterator over words in csvfile"""
+        try:
+            csvfile = open(self.filename)
+        except IOError:
+            print("can't open csv file")
+        else:
+            csvreader = reader(csvfile)
+            if self.header == True:
+                next(csvreader)
+            for row in csvreader:
+                if self.firstcol == "mylang":
+                    yield (row[0], row[1])
+                else:
+                    yield (row[1], row[0])
+        
+
--- subtitles_translator.py
+++ subtitles_translator.py
@@ -1,7 +1,12 @@
 from srt_parser import parse_srt
+from csv_importer import csv_importer
 
 if __name__ == "__main__":
-    srt_file = open("example_srt_file.txt", "r")
-    srt_words = parse_srt(srt_file)
-    for word in srt_words:
-        print(word)
+    # srt_file = open("example_srt_file.txt", "r")
+    # srt_words = parse_srt(srt_file)
+    # for word in srt_words:
+    #     print(word)
+    
+    anki = csv_importer("testAnki.csvd")
+    for word in anki.read():
+        print( word[0] + " - " + word[1])
