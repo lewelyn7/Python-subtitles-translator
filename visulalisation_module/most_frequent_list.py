393dd385aa1c8085451afcb802d82fae4986ded9
retkiewi <58555777+retkiewi@users.noreply.github.com>
2020-05-11 23:27:09 +0200

    fixed callback problems in list classes

--- visulalisation_module/most_frequent_list.py
+++ visulalisation_module/most_frequent_list.py
@@ -1,42 +1,42 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from most_frequent_list_UI import Ui_MostFrequentList
+from most_frequent_list_UI import Ui_mostFrequentList
 
-class MostFrequentController(QtWidgets.QMainWindow):
+class MostFrequentController(QtWidgets.QDialog):
     wordList = []
     iterator = 0
     def __init__(self, wordList):
         super().__init__()
         self.wordList = wordList
-        self.ui = Ui_MostFrequentList()
+        self.ui = Ui_mostFrequentList()
         self.ui.setupUi(self)
-        self.ui.word_label.setText(self.wordList[0][0])
+        self.ui.word_label_2.setText(self.wordList[0][0])
         self.show()
 
     def translateWord(self):
-        isTranslated = self.wordList[self.iterator].index(self.ui.word_label.text())
-        self.ui.word_label.setText(self.wordList[self.iterator][(isTranslated+1)%2])
+        isTranslated = self.wordList[self.iterator].index(self.ui.word_label_2.text())
+        self.ui.word_label_2.setText(self.wordList[self.iterator][(isTranslated+1)%2])
     
     def nextWord(self):
         if self.iterator < len(self.wordList)-1:
             self.iterator+=1
-        self.ui.word_label.setText(self.wordList[self.iterator][0])
+        self.ui.word_label_2.setText(self.wordList[self.iterator][0])
         if self.wordList[self.iterator][2] == 0:
-            self.ui.not_known_button.setChecked(True)
+            self.ui.not_known_button_2.setChecked(True)
         else:
-            self.ui.known_button.setChecked(True)
+            self.ui.known_button_2.setChecked(True)
 
     def previousWord(self):
         if self.iterator > 0:
             self.iterator-=1
-        self.ui.word_label.setText(self.wordList[self.iterator][0])
+        self.ui.word_label_2.setText(self.wordList[self.iterator][0])
         if self.wordList[self.iterator][2] == 0:
-            self.ui.not_known_button.setChecked(True)
+            self.ui.not_known_button_2.setChecked(True)
         else:
-            self.ui.known_button.setChecked(True)
+            self.ui.known_button_2.setChecked(True)
     
     def updateScore(self):
-        if self.ui.known_button.isChecked():
+        if self.ui.known_button_2.isChecked():
             self.wordList[self.iterator][2] = 1
         else:
             self.wordList[self.iterator][2] = 0
@@ -44,13 +44,15 @@ class MostFrequentController(QtWidgets.QMainWindow):
     
     def saveAndExit(self):
         self.close()
-        print(self.wordList)
         return self.wordList
         
-
+    def getList(self):
+        return self.wordList
     
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     load_srt = MostFrequentController([['apple', 'japko', 0], ['pear', 'gruszka', 0]])
-    sys.exit(app.exec_())
+    app.exec_()
+    list1 = load_srt.getList()
+    print(list1)
--- visulalisation_module/no_translation_list.py
+++ visulalisation_module/no_translation_list.py
@@ -1,31 +1,69 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from most_frequent_list_UI import Ui_MostFrequentList
+from no_translation_list_UI import Ui_noTranslationList
 
-class MostFrequentController(QtWidgets.QMainWindow):
+class NoTranslationController(QtWidgets.QDialog):
     wordList = []
     iterator = 0
     def __init__(self, wordList):
         super().__init__()
         self.wordList = wordList
-        self.ui = Ui_NoTranslationList()
+        self.ui = Ui_noTranslationList()
         self.ui.setupUi(self)
         self.ui.word_label.setText(self.wordList[0][0])
         self.show()
+
     
     def nextWord(self):
         if self.iterator < len(self.wordList)-1:
             self.iterator+=1
         self.ui.word_label.setText(self.wordList[self.iterator][0])
+        self.ui.translationText.clear()
 
     def previousWord(self):
         if self.iterator > 0:
             self.iterator-=1
         self.ui.word_label.setText(self.wordList[self.iterator][0])
+        self.ui.translationText.clear()
 
+    def saveAndExit(self):
+        output = []
+        for word in self.wordList:
+            if len(word)!=1:
+                output.append(word)
+        self.wordList = output
+        self.close()
+        
+
+
+    def addTranslation(self):
+        translation = self.ui.translationText.toPlainText()
+        if len(translation) == 0:
+            return
+        if translation not in self.wordList[self.iterator]:
+            self.wordList[self.iterator].append(translation)
+        self.ui.translationText.clear()
+    
+    def deleteWord(self):
+        if len(self.wordList) <= 1:
+            self.saveAndExit()
+            return
+
+        self.wordList.remove(self.wordList[self.iterator])
+
+        if self.iterator >= len(self.wordList)-1:
+            self.iterator-=1
+
+        self.ui.word_label.setText(self.wordList[self.iterator][0])
+        
+    def getList(self):
+        return self.wordList
     
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
-    load_srt = MostFrequentController([['apple', ''], ['pear', '']])
+    load_srt = NoTranslationController([['apple'], ['pear'], ['orange'], ['pineaple']])
     sys.exit(app.exec_())
+    list1 = load_srt.getList()
+    print(list1)
+    
